2593575ac9846272e0a219b9404d26d0cf2c85a1
Luciano Braga <lucianobragaweb@gmail.com>
2019-03-02 12:19:55 -0300

    Setando operações e digitos

--- src/App.js
+++ src/App.js
@@ -7,28 +7,41 @@ export default class App extends Component {
   state = {
     displayValue: '0'
   }
+
+  addDigit = n => {
+    this.setState({ displayValue: n })
+  }
+
+  clearMemory = () => {
+    this.setState({ displayValue: '0'})
+  }
+
+  setOperation = operation => {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Display value={this.state.displayValue}/>
         <View style={styles.buttons}>
-          <Button label='AC' />
-          <Button label='/' />
-          <Button label='7' />
-          <Button label='8' />
-          <Button label='9' />
-          <Button label='*' />
-          <Button label='4' />
-          <Button label='5' />
-          <Button label='6' />
-          <Button label='-' />
-          <Button label='1' />
-          <Button label='2' />
-          <Button label='3' />
-          <Button label='+' />
-          <Button label='0' />
-          <Button label='.' />
-          <Button label='=' />
+          <Button label='AC'onClick={this.clearMemory} triple/>
+          <Button label='/' onClick={this.setOperation} operation/>
+          <Button label='7' onClick={this.addDigit}/>
+          <Button label='8' onClick={this.addDigit}/>
+          <Button label='9' onClick={this.addDigit}/>
+          <Button label='*' onClick={this.setOperation} operation/>
+          <Button label='4' onClick={this.addDigit}/>
+          <Button label='5' onClick={this.addDigit}/>
+          <Button label='6' onClick={this.addDigit}/>
+          <Button label='-' onClick={this.setOperation} operation/>
+          <Button label='1' onClick={this.addDigit}/>
+          <Button label='2' onClick={this.addDigit}/>
+          <Button label='3' onClick={this.addDigit}/>
+          <Button label='+' onClick={this.setOperation} operation/>
+          <Button label='0' onClick={this.addDigit} double/>
+          <Button label='.' onClick={this.addDigit}/>
+          <Button label='=' onClick={this.setOperation}/>
         </View>
       </View>
     );
