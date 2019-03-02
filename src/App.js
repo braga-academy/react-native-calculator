3a04dfe3142dfcce1244192a8a79bd76a6507735
Luciano Braga <lucianobragaweb@gmail.com>
2019-03-02 13:53:44 -0300

    Implementando operações e corrigindo erros

--- src/App.js
+++ src/App.js
@@ -8,29 +8,30 @@ const initialState = {
   clearDisplay: false,
   operation: null,
   values: [0, 0],
-  current: 0
+  current: 0,
 }
 
 export default class App extends Component {
   state = { ...initialState }
 
   addDigit = n => {
-    if(n === '.' && this.state.displayValue.includes('.')) {
-      return
-    }
-
     const clearDisplay = this.state.displayValue === '0'
       || this.state.clearDisplay
 
+    if (n === '.' && !clearDisplay
+      && this.state.displayValue.includes('.')) {
+      return
+    }
+
     const currentValue = clearDisplay ? '' : this.state.displayValue
     const displayValue = currentValue + n
-
     this.setState({ displayValue, clearDisplay: false })
 
-    if(n !== '.') {
+    if (n !== '.') {
       const newValue = parseFloat(displayValue)
       const values = [...this.state.values]
       values[this.state.current] = newValue
+      this.setState({ values })
     }
   }
 
@@ -39,7 +40,29 @@ export default class App extends Component {
   }
 
   setOperation = operation => {
+    if(this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
 
+      try {
+        values[0] =
+          eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (e) {
+        values[0] = this.state.values[0]
+      }
+
+      values[1] = 0
+
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      })
+    }
   }
 
   render() {
